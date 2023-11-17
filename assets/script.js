826eb581126948be43579e677624a5c6a3ae8caf
Anna Chuapetcharasopon <143050387+ahgeak@users.noreply.github.com>
2023-11-17 08:24:15 -0800

    HTML: added header, main and body elements; JS: fixed API call functions, used params to pass in data, removed extra variables, fixed variable scopes from global to local

--- assets/script.js
+++ assets/script.js
@@ -4,113 +4,115 @@
 // pass user input into API to fetch data
 // display current weather data
 // update and create HTML elements based on 5 day weather forecast
-// Local storage to save a search history. 
+// Local storage to save a search history.
 // when save city is clicked update HTML elements on the page
 
 // current weather API documentation: https://openweathermap.org/current#name
 // API call link: https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 var weatherApiKey = "83bc9be355d058eefcfe18d969db5d21";
-var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?";
-var enteredCityLat = "47.6038321"; // get this from geocode API
-var enteredCityLon = "-122.330062"; // get this from geocode API
 
-var currentWeatherApi = `${weatherUrl}lat=${enteredCityLat}&lon=${enteredCityLon}&appid=${weatherApiKey}`
-console.log(currentWeatherApi);
-
+var searchBtn = document.querySelector("#search");
+var userInput = document.querySelector("#input");
+var currentWeatherContainer = document.querySelector("#current");
+var searchHistory = JSON.parse(localStorage.getItem('cities')) || []
 // geocode API documentation: https://openweathermap.org/api/geocoding-api
 // API call link: http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-var geocodeUrl = "http://api.openweathermap.org/geo/1.0/direct?q=";
-var enteredCity = "seattle"; // get this from the city input section
-var enteredState = "wa";
-var countryCode = "us";
 
-var geocodeApi = `${geocodeUrl}${enteredCity},${enteredState},${countryCode}&limit=5&appid=${weatherApiKey}`
+// // forecast API documentation: https://openweathermap.org/forecast16
+// // API call link: api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+// var forecastUrl = "http://api.openweathermap.org/data/2.5/forecast?";
 
-// forecast API documentation: https://openweathermap.org/forecast16
-// API call link: api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-var forecastUrl = "http://api.openweathermap.org/data/2.5/forecast?";
+// var forecastApi = `${forecastUrl}lat=${enteredCityLat}&lon=${enteredCityLon}&appid=${weatherApiKey}`
+// console.log(forecastApi);
 
-var forecastApi = `${forecastUrl}lat=${enteredCityLat}&lon=${enteredCityLon}&appid=${weatherApiKey}`
-console.log(forecastApi);
-
-var longitude;
-var latitude;
-
-function fetchGeocode(){
-    fetch(geocodeApi)
-        .then (function(response) {
-            // console.log(response);
-            return response.json();
-        })
-        .then (function (data) {
-            // console.log(data);
-            latitude = data[0].lat;
-            // console.log(latitude);
-            longitude = data[0].lon
-            // console.log(longitude);
-            return latitude, longitude;
-        })
-        .catch(function (err) {
-            console.error(err);
-        });
-}
-console.log("this is the lat " + latitude); // this comes back undefined
-
-var currentDate; // I need to figure out how to do the date element
-var currentTemp;
-var currentWind;
-var currentHumidity;
-
-function fetchWeather(){
-    fetch(currentWeatherApi)
-        .then (function(response) {
-            // console.log(response);
-            return response.json();
-        })
-        .then (function (data) {
-            console.log(data);
-            currentTemp = data.main.temp;
-            console.log(currentTemp); //Why is this 280 degrees?
-            currentWind = data.wind.speed;
-            console.log(currentWind);
-            console.log(data.weather[0].icon) //Not sure if this can connect to icon later? Need to research
-            currentHumidity = data.main.humidity;
-            console.log(currentHumidity);
-        })
-        .catch(function (err) {
-            console.error(err);
-        });
+function start() {
+  var city = userInput.value;
+  fetchGeocode(city);
 }
 
-function fetchForecast(){
-    fetch(forecastApi)
-        .then (function(response) {
-            // console.log(response);
-            return response.json();
-        })
-        .then (function (data) {
-            console.log(data);
-            // next I need to get the 5 day forecast to display on the screen
-        })
-        .catch(function (err) {
-            console.error(err);
-        });
+function fetchGeocode(city) {
+  var geocodeUrl = "http://api.openweathermap.org/geo/1.0/direct?q=";
+
+  var geocodeApi = `${geocodeUrl}${city}&limit=5&appid=${weatherApiKey}`;
+  fetch(geocodeApi)
+    .then(function (response) {
+      // console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      // console.log(data);
+      var latitude = data[0].lat;
+      // console.log(latitude);
+      var longitude = data[0].lon;
+      // console.log(longitude);
+      fetchWeather(latitude, longitude);
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
+}
+// console.log("this is the lat " + latitude); // this comes back undefined
+
+function fetchWeather(lat, lon) {
+  var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherApiKey}&units=imperial`;
+
+  fetch(weatherUrl)
+    .then(function (response) {
+      // console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+        currentWeatherContainer.innerHTML = ''
+      var cityNameEl = document.createElement("h2");
+
+      cityNameEl.textContent = data.name;
+
+      currentWeatherContainer.append(cityNameEl);
+      console.log(data);
+      var currentDate = new Date(data.dt * 1000).toLocaleDateString();
+      var currentTemp = data.main.temp;
+
+      var iconcode = data.weather[0].icon;
+      console.log(currentTemp); //Why is this 280 degrees?
+      var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+
+      //    document.querySelector('#icon').setAttribute('src', iconurl)
+
+      // currentWind = data.wind.speed;
+      // console.log(currentWind);
+      // console.log(data.weather[0].icon) //Not sure if this can connect to icon later? Need to research
+      // currentHumidity = data.main.humidity;
+      // console.log(currentHumidity);
+      saveToStorage(data.name)
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
 }
 
-fetchGeocode();
-
-fetchWeather();
-
-fetchForecast();
-
-function currentWeather() {
+function fetchForecast() {
+  fetch(forecastApi)
+    .then(function (response) {
+      // console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      // next I need to get the 5 day forecast to display on the screen
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
 }
 
-function fiveDayForecast() {
+function saveToStorage(city){
+// need to save the cities that the user types in into local storage
+if(!searchHistory.includes(city)){
+    searchHistory.push(city)
+    localStorage.setItem('cities', JSON.stringify(searchHistory))
+
 }
 
-function initializePage () {
 }
 
-function clearForecast (){
-}
+searchBtn.addEventListener("click", start);
