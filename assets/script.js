1c39c970b970406b91610a26eee76915698ff70c
Anna Chuapetcharasopon <143050387+ahgeak@users.noreply.github.com>
2023-11-19 19:03:53 -0800

    HTML: Added bootstrap styling; JS: added comments and cleaned up code

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,27 @@
-// Psudo code:
-// Get user input using event listener this will call currentWeather and fiveDayForecast functions maybe clearForecast
-// pass user input into API to fetch data
-// display current weather data
-// update and create HTML elements based on 5 day weather forecast
-// Local storage to save a search history.
-// when save city is clicked update HTML elements on the page
-
 // current weather API documentation: https://openweathermap.org/current#name
-// API call link: https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-var weatherApiKey = "83bc9be355d058eefcfe18d969db5d21";
+// geocode API documentation: https://openweathermap.org/api/geocoding-api
+// // forecast API documentation: https://openweathermap.org/forecast16
 
+// Global variables
+var weatherApiKey = "83bc9be355d058eefcfe18d969db5d21";
 var searchBtn = document.querySelector("#search");
 var userInput = document.querySelector("#input");
 var currentWeatherContainer = document.querySelector("#current");
 var historyContainer = document.querySelector("#history");
 var forecastWeatherContainer = document.querySelector("#forecast");
 var searchHistory = JSON.parse(localStorage.getItem("cities")) || [];
-// geocode API documentation: https://openweathermap.org/api/geocoding-api
-// API call link: http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 
-// // forecast API documentation: https://openweathermap.org/forecast16
-// // API call link: api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-// var forecastUrl = "http://api.openweathermap.org/data/2.5/forecast?";
-
-// var forecastApi = `${forecastUrl}lat=${enteredCityLat}&lon=${enteredCityLon}&appid=${weatherApiKey}`
-// console.log(forecastApi);
-
+// Start funciton takes the user input and passes city into API fetch function
 function start() {
   var city = userInput.value;
   fetchGeocode(city);
 }
 
+// fetchGeocode uses the inputed city to get the longitude and lattidue and pass thoses into the Weather and Forecast APIs
 function fetchGeocode(city) {
   var geocodeUrl = "http://api.openweathermap.org/geo/1.0/direct?q=";
-
   var geocodeApi = `${geocodeUrl}${city}&limit=5&appid=${weatherApiKey}`;
+
   fetch(geocodeApi)
     .then(function (response) {
       return response.json();
@@ -49,8 +36,8 @@ function fetchGeocode(city) {
       console.error(err);
     });
 }
-// console.log("this is the lat " + latitude); // this comes back undefined
 
+// fetchWeaher will take the latitude and longitude into the WeatherAPI and display that on the page
 function fetchWeather(lat, lon) {
   var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherApiKey}&units=imperial`;
 
@@ -62,18 +49,15 @@ function fetchWeather(lat, lon) {
       currentWeatherContainer.innerHTML = "";
       var cityNameEl = document.createElement("h2");
       var currentDate = dayjs().format("M/D/YYYY"); //use DayJS
-      // console.log(currentDate);
-      // var currentDate = new Date(data.dt * 1000).toLocaleDateString();
-
-      cityNameEl.textContent = `${data.name} (${currentDate})`;
-      currentWeatherContainer.append(cityNameEl);
-
       var currentTemp = data.main.temp;
       var currentWind = data.wind.speed;
       var currentHumidity = data.main.humidity;
-
       var iconcode = data.weather[0].icon;
       var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+
+      // Display the searched city on the page, update the HTML elements with the temp, wind, humidity and icon
+      cityNameEl.textContent = `${data.name} (${currentDate})`;
+      currentWeatherContainer.append(cityNameEl);
 
       document.querySelector("#icon").setAttribute("src", iconurl);
       document.querySelector(
@@ -91,10 +75,10 @@ function fetchWeather(lat, lon) {
     });
 }
 
+// fetchForecast is passed in lat and lon to get the 5 day forecast data to pass into displayForecast function
 function fetchForecast(lat, lon) {
   var forecastUrl = "http://api.openweathermap.org/data/2.5/forecast?";
   var forecastApi = `${forecastUrl}lat=${lat}&lon=${lon}&units=imperial&appid=${weatherApiKey}`;
-  console.log(forecastApi);
 
   fetch(forecastApi)
     .then(function (response) {
@@ -102,8 +86,6 @@ function fetchForecast(lat, lon) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data.list);
-      // console.log(data.list[2].dt_txt);
       displayForecast(data.list);
     })
     .catch(function (err) {
@@ -111,27 +93,26 @@ function fetchForecast(lat, lon) {
     });
 }
 
+// displayForecast will take the data from the forecastAPI and use DayJS to display the data on the page
 function displayForecast(dailyForecast) {
   var startDate = dayjs().add(1, "day").startOf("day").unix();
   var endDate = dayjs().add(6, "day").startOf("day").unix();
-  // use DayJS instead of new Date
 
-  // next I need to get the 5 day forecast to display on the screen
+  // This for loop will take the length of dailyForecast to iterate through
   for (var i = 0; i < dailyForecast.length; i++) {
+    // This if statement will determine if the current date is within the range of the 5 days to be displayed
     if (dailyForecast[i].dt > startDate && dailyForecast[i].dt < endDate) {
+      // This if statment will ensure that only the data for the noon forecast is displayed to prevent repeat days
       if (dailyForecast[i].dt_txt.slice(11, 13) === "12") {
-        // console.log(dailyForecast[i]);
         createForecastCard(dailyForecast[i]);
       }
     }
   }
 }
 
+// createForecastCard function will create the cards for each of the 5-day forecast cards
 function createForecastCard(forecast) {
-  // console.log(forecast.dt_txt);
   var day = dayjs(forecast.dt_txt).format("dddd");
-  // console.log(day);
-
   var iconCode = forecast.weather[0].icon;
   var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
   var forecastTemp = forecast.main.temp;
@@ -157,29 +138,36 @@ function createForecastCard(forecast) {
   windEl.textContent = `Wind: ${forecastWind} MPH`;
   humidityEl.textContent = `Humidity: ${forecastHumidity}%`;
 
-  // cardContainerEl.setAttribute("class", "row");
+  // cardContainerEl.setAttribute("class", "col");
   // cardEl.setAttribute("class", "col");
 
+  cardContainerEl.setAttribute(
+    "class",
+    "border border-primary rounded m-1 border-5 text-center"
+  );
   forecastWeatherContainer.append(cardContainerEl);
 }
 
+// saveToStorage will save the searched cities to localStorage and display the city names on the screen with a button
 function saveToStorage(city) {
-  // need to save the cities that the user types in into local storage
   if (!searchHistory.includes(city)) {
+    // Sets the searched items to local storage
     searchHistory.push(city);
     localStorage.setItem("cities", JSON.stringify(searchHistory));
-    
-    // history.innerHTML = "";
+
+    // Displays the searched city buttons on the screen
     var historyEl = document.createElement("div");
     var titleEl = document.createElement("button");
     historyEl.append(titleEl);
     titleEl.textContent = city;
+
+    // titleEl.setAttribute("class", "btn btn-secondary m-2");
     // console.log(city);
     historyContainer.append(historyEl);
-    return titleEl.value;
   }
 }
 
+// This function is to call the searched cities back
 // function searchButtonClicked(citySearched) {
 //   var city = userInput.value;
 //   fetchGeocode(citySearched);
